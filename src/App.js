5450afdf49a7c0244c8dc838c5ec817910f5a6ff
nobusugahara-japan <nobu.sugahara@b2bmakers.jp>
2023-01-16 09:48:40 +0900

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const answers = ["アイテム１", "アイテム２", "アイテム３", "アイテム４"];
+  const questions = ["今日の気分はどうですか?", "明日の予定は何ですか？"]
+  const [val, setVal] = useState("アイテム1");
+  const handleChange = (e) => {
+    setVal(e.target.value);
+  };
+
+  const [questionState, setQuestionState] = useState(0)
+
+  const nextPage = ()=>{
+    setQuestionState(questionState+1);
+  }
+  const backPage = () =>{
+    setQuestionState(questionState-1);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{questions[questionState]}</p>
+      <div>
+        {answers.map((answer) => {
+          return (
+            <div key={answer}>
+              <input
+                id={answer}
+                type="radio"
+                value={answer}
+                onChange={handleChange}
+                checked={answer === val}
+              />
+              <label htmlFor={answer}>{answer}</label>
+            </div>
+          );
+        })}
+      <div className="App">
+        <div className="inner">
+          <button onClick={backPage}>戻る</button>
+          <button onClick={nextPage}>進む</button>
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
