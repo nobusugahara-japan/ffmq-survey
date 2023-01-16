0d1b81f8d27e8d5abbb3f9c0f60dbd9f046ad800
nobusugahara-japan <nobu.sugahara@b2bmakers.jp>
2023-01-16 12:06:17 +0900

    4th

--- src/App.js
+++ src/App.js
@@ -6,12 +6,18 @@ function App() {
   const questions = ["瞑想は毎日行いますか?", "瞑想すると気分が良くなりますか？",
                     "イライラすることが多いですか？", "自分が怒っている時にその感情に気付きますか？",
                     "不安なことが多いですか？"]
-  const [val, setVal] = useState("アイテム1");
+
+  const [val, setVal] = useState("瞑想は毎日行いますか");
+
   const handleChange = (e) => {
     setVal(e.target.value);
+    setTimeout(() => {
+      setQuestionState(questionState+1);
+      setVal("")
+    }, 500);
   };
 
-  const [questionState, setQuestionState] = useState(0)
+  const [questionState, setQuestionState] = useState(-1)
 
   const nextPage = ()=>{
     setQuestionState(questionState+1);
@@ -19,33 +25,56 @@ function App() {
   const backPage = () =>{
     setQuestionState(questionState-1);
   }
-  return (
-    <div className="App">
-      <p>{questions[questionState]}</p>
-      <div>
-        {answers.map((answer) => {
-          return (
-            <div key={answer}>
-              <input
-                id={answer}
-                type="radio"
-                value={answer}
-                onChange={handleChange}
-                checked={answer === val}
-              />
-              <label htmlFor={answer}>{answer}</label>
-            </div>
-          );
-        })}
-      <div className="App">
-        <div className="inner">
-          <button onClick={backPage}>戻る</button>
-          <button onClick={nextPage}>進む</button>
-        </div>
-      </div>
-    </div>
-    </div>
-  );
-}
 
+  const fixResult = () =>{
+    console.log(val)
+  }
+
+  console.log(questionState);
+
+  if (questionState<0){
+    return(
+      <div className="App">
+        <h2>サーベーを開始してください</h2>
+            <button onClick={nextPage}>進む</button>
+      </div>
+    )
+  } else if (questionState===5){
+    return(
+    <div className="App">
+      <h2>確定してよろしいですか？</h2>
+      <button
+        onClick={fixResult}
+      >確定</button>
+    </div>
+  )}
+  else{
+      return (
+        <div className="App">
+          <div>
+          <p>{questions[questionState]}</p>
+            {answers.map((answer) => {
+              return (
+                <div key={answer}>
+                  <input
+                    id={answer}
+                    type="radio"
+                    value={answer}
+                    onChange={handleChange}
+                    checked={answer === val}
+                  />
+                  <label htmlFor={answer}>{answer}</label>
+                </div>
+              );
+            })}
+          </div>
+          <div className="App">
+            <div className="inner">
+              <button onClick={backPage}>戻る</button>
+            </div>
+          </div>
+        </div>
+      );
+    }
+}
 export default App;
